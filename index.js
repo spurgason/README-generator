ae060424a5beaa2fdd412873e4e4d71fe0303aca
spurgason <shanepurgason.98@gmail.com>
2021-09-23 15:24:13 -0400

    Badge Renders

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'What license does this project use?',
-        choices: ['MIT', 'Apache 2.0', 'GNU General Public License (GPL) 2.0', 'GNU General Public License (GPL) 3.0']
+        choices: ['MIT', 'Apache 2.0', 'GNU General Public License (GPL) 2.0', 'GNU General Public License (GPL) 3.0', 'None']
     },
     {
         type: 'input',
@@ -63,6 +63,7 @@ function init() {
     inquirer.prompt(questions)
         .then((data) => {
            writeToFile('README.md', generateMarkdown(data))
+
         })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
- 
+function renderLicenseBadge(data) {
+  const licenseInput = data.license
+  if (licenseInput == 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (licenseInput == 'Apache 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (licenseInput == 'GNU General Public License (GPL) 2.0') {
+    return  `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+  } else if (licenseInput == 'GNU General Public License (GPL) 3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function that returns the license link
@@ -16,6 +27,7 @@ function renderLicenseSection(license) { }
 function generateMarkdown(data) {
   return `
   
+  ${renderLicenseBadge(data)} 
   # ${data.title} 
   
   
