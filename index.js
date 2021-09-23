eb507e7a1b5800a9033af5c6ceb551782919cc4e
spurgason <shanepurgason.98@gmail.com>
2021-09-23 11:46:15 -0400

    Added Questions and Packages

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
-const generateMarkdown = require('generateMarkdown')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of this project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Add a discription for this project.'
+    },
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: 'Add your table of contents here.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install this project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'How do you use this project?'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who contributed to this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can users text this product?'
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
 init();
